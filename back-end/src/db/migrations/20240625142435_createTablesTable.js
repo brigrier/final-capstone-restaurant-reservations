b2d367a394a4e665a198a07b7ad5f5a9f7837ece
Bri Grier <brianna.g6879@gmail.com>
2024-06-25 14:29:23 -0500

    createTables migration finshed

--- back-end/src/db/migrations/20240625142435_createTablesTable.js
+++ back-end/src/db/migrations/20240625142435_createTablesTable.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("tables", (table) => {
+        table.increments("table_id").primary();
+        table.timestamps(true, true).notNullable();
+        table.string("table_name").notNullable();
+        table.integer("people").notNullable();
+      });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable("tables");
 };
