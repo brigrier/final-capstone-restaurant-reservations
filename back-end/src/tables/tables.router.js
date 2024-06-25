d787235830619476e54e00253a8f6be1e877c417
Bri Grier <brianna.g6879@gmail.com>
2024-06-25 14:19:07 -0500

    connecting tables files

---
+++ back-end/src/tables/tables.router.js
@@ -0,0 +1,10 @@
+/**
+ * Defines the router for reservation resources.
+ *
+ * @type {Router}
+ */
+const router = require("express").Router();
+const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed")
+
+module.exports = router;
