edde9482710dcb0d8348512fb4fe265f022c00e6
Bri Grier <brianna.g6879@gmail.com>
2024-06-26 11:36:22 -0500

    drafted create functions in /tables path, no validation yet

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -6,6 +6,17 @@ async function list(req, res) {
   res.json({ data });
 }
 
+// POST
+async function create(req, res, next) {
+    try {
+      const data = await service.create(req.body.data);
+      res.status(201).json({ data });
+    } catch (error) {
+      next(error);
+    }
+  }
+
 module.exports = {
   list,
+  create
 };
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -5,6 +5,16 @@ function list() {
   return knex("tables").select("*");
 }
 
+//POST
+function create(table) {
+    return knex("tables")
+    .insert(table)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+
 module.exports = {
-    list
+    list,
+    create
 }
