81c1fba11688d7a29e83ae16e108c16e416bef61
Bri Grier <brianna.g6879@gmail.com>
2024-06-27 13:00:00 -0500

    user story 7 input

--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 
 const Search = () => {
-    return (
-        
-    )
-}
+  return (
+    <div class="input-group">
+      <input
+        type="search"
+        name="mobile_number"
+        class="form-control rounded"
+        placeholder="Enter a customer's phone number"
+        aria-label="Search"
+        aria-describedby="search-addon"
+      />
+      <button
+        type="button"
+        class="btn btn-outline-primary"
+        data-mdb-ripple-init
+      >
+        Find
+      </button>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
