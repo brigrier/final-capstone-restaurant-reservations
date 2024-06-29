d8260d87d8b4b7105f14b91d20af877629c72f66
Bri Grier <brianna.g6879@gmail.com>
2024-06-28 21:16:59 -0500

    added column to tables

--- back-end/src/db/migrations/20240626160557_createTablesTable.js
+++ back-end/src/db/migrations/20240626160557_createTablesTable.js
@@ -3,6 +3,7 @@ exports.up = function (knex) {
       table.increments("table_id").primary();
       table.string("table_name").notNullable();
       table.integer("capacity").notNullable();
+      table.integer("reservation_id");
       table.timestamps(true, true);
     });
   };
